2a728905d0be4d9084e8917874e3cc5e0b8aa982
Shaxadhere <Shaxad.here@gmail.com>
2021-03-24 10:43:59 +0500

    express methods

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -2,5 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    
+    res.send("hello world from node js")
 })
+
+const port = 8080;
+app.listen(port, () => {console.log(`Listening on ${port}`)})
