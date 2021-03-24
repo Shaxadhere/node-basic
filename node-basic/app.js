61cc4759532818927b78d5ecb94d077da359a5cf
Shaxadhere <Shaxad.here@gmail.com>
2021-03-24 10:05:03 +0500

    express js

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end("hello from browser");
+    res.end("hello from browser updated");
 });
 
 server.listen(3000)
