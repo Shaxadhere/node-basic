8b947140f39548aadded4a4eed4226260b53b665
Shaxadhere <Shaxad.here@gmail.com>
2021-03-24 12:20:21 +0500

    database

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -5,6 +5,14 @@ const morgan = require('morgan')
 
 const connectString = "mongodb+srv://admin:dGakD2MeWQprWN02@cluster0.stgq9.mongodb.net/tinderdb?retryWrites=true&w=majority";
 
+mongoose.connect(connectString, {useUnifiedTopology: true, useNewUrlParser: true })
+.then(() => console.log("Connected to database"))
+
+mongoose.connection.on('error', err => {
+    console.log(`Database connection error: ${err.message}`)
+})
+
+
 //Middlewares
 app.use(morgan("dev"))
 
