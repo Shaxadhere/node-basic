1cdf07499f722c93b89b2233da8c02cde4c0499c
Shaxadhere <Shaxad.here@gmail.com>
2021-03-24 13:18:12 +0500

    post validation

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -3,21 +3,19 @@ const Post = require('../models/post')
 exports.getPosts = (req, res) => {
     res.json({
         posts: [
-            {title: 'First Post'}, 
-            {title: 'Second Post'}
+            {
+                title: 'First Post'
+            }, {
+                title: 'Second Post'
+            }
         ]
     })
 }
 
 exports.createPost = (req, res) => {
     const post = new Post(req.body);
-    // console.log("Creating Post: ", req.body)
-    post.save((err, result) => {
-        if(err){
-            return res.status(400).json({
-                error: err
-            })
-        }
+    
+    post.save().then(result => {
         res.status(200).json({
             post: result
         })
