dbc3034a063f866a8ec012fcef80ba7db0816274
Shaxadhere <Shaxad.here@gmail.com>
2021-03-24 12:12:17 +0500

    mongoose

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const morgan = require('morgan')
 
+const connectString = "mongodb+srv://admin:dGakD2MeWQprWN02@cluster0.stgq9.mongodb.net/tinderdb?retryWrites=true&w=majority";
 
 //Middlewares
 app.use(morgan("dev"))
