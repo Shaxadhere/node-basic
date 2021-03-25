5c27feb48d5061de7a807af8080589b9855a85fe
Shaxadhere <Shaxad.here@gmail.com>
2021-03-25 12:01:20 +0500

    api crud finished

--- api/routes/post.js
+++ api/routes/post.js
@@ -6,10 +6,12 @@ const router = express.Router()
 
 router.get('/', postController.getPosts)
 
-router.post('/post', postController.CreatePost)
+router.post('/post', validator.createPostValidator, postController.CreatePost)
 
 router.get('/post', postController.ViewPost)
 
-router.put('/post', postController.UpdatePost)
+router.put('/post', validator.createPostValidator, postController.UpdatePost)
+
+router.delete('/post', postController.DeletePost)
 
 module.exports = router;
--- api/validator/index.js
+++ api/validator/index.js
@@ -1,4 +1,4 @@
-exports.createPostValidator = (req, res) => {
+exports.createPostValidator = (req, res, next) => {
     req.check('title', "Write a title").notEmpty()
     req.check('title', 'Title must be between 4 to 140 charecters').isLength({
         min: 4, max: 140
@@ -9,5 +9,12 @@ exports.createPostValidator = (req, res) => {
         min: 4, max:1000
     })
 
-    
+    const errors = req.validationErrors();
+
+    if(errors){
+        const firstError = errors.map((error) => error.message)[0]
+        return res.status(400).json({error: firstError})
+    }
+
+    next()
 }
