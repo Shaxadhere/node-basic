d7c0b3694cbbd8d3f8043ea3f61b2eeaed6e6a7b
Shaxadhere <Shaxad.here@gmail.com>
2021-03-25 09:35:59 +0500

    cors

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
 const morgan = require('morgan')
@@ -6,26 +7,35 @@ const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
 
 
-//Connecting to database
+// Connecting to database
 const connectString = "mongodb+srv://admin:dGakD2MeWQprWN02@cluster0.stgq9.mongodb.net/tinderdb?retryWrites=true&w=majority";
 
-mongoose.connect(connectString, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false })
-.then(() => console.log("Connected to database"))
+mongoose.connect(connectString, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+}).then(() => console.log("Connected to database"))
 
 mongoose.connection.on('error', err => {
-    console.log(`Database connection error: ${err.message}`)
+    console.log(`Database connection error: ${
+        err.message
+    }`)
 })
 
 
-//Middlewares
+// Middlewares
+app.use(cors())
 app.use(morgan("dev"))
 app.use(bodyParser.json())
 app.use(expressValidator())
 
-//Adding post routes
+
+// Adding post routes
 const postRoutes = require('./routes/post')
 app.use('/', postRoutes)
 
-//Configuring port
+// Configuring port
 const port = 8080;
-app.listen(port, () => {console.log(`Listening on ${port}`)})
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+})
